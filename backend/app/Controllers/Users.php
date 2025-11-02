46d65f3b4e245e7ee1b293ae5929187f889e7d2d
Jayveepnl <panoljayvee2@gmail.com>
2025-11-03 01:57:42 +0800

    feat(frontend/AccountPage): Added Account page for admin

--- backend/app/Controllers/Users.php
+++ backend/app/Controllers/Users.php
@@ -35,4 +35,8 @@ class Users extends BaseController
     {
         return view('users/ServicePage');
     }
+    public function AccountPage()
+    {
+        return view('users/AccountPage');
+    }
 }
